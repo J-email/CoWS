b390f9aa54aa02749601528e8cc496e85c4fb901
jerry <email__address@163.com>
2021-11-16 18:03:59 +0800

    better colorado river decrease rate

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 import numpy as np
 import scipy.stats as stats
 from matplotlib import pyplot as plt
+from ml_metrics import rmse
 
 volumeB = [0.153, 1.00700, 2.00600, 3.15600, 4.47500, 5.98100, 7.68300, 9.60100, 11.73500, 14.09200, 16.73300, 19.72800, 23.14400, 26.973000]
 areaB = [31.20, 37.10, 42.90, 49.20, 56.40, 64.10, 72.20, 81.10, 89.30, 99.30, 112.30, 128.40, 144.80, 160.70]
@@ -24,8 +25,8 @@ def population(year):
 
 def coloradoRiver(year):
     if currentYear < 2000:
-        return 9.8
-    return 9.5 * (0.8 ** 0.01 ** (currentYear - 2000))
+        return 10
+    return 9.2 * (0.8 ** 0.01 ** (currentYear - 2000))
 
 
 def lasVegasWash(year):
@@ -58,29 +59,39 @@ def evaporation(year):
     return 80/12 * volume_to_area(lastVolume) / 1000
 
 
-# Press the green button in the gutter to run the script.
+def water_level(year):
+    if year == 2021:
+        return 10.177
+    return water_level(year - 1/2048) + (inflow(year) - outflow(year) - evaporation(year))/2048
+
+
 if __name__ == '__main__':
     step = 1/2048
-    startYear = 1980
+    startYear = 2020
     startPopulation = 2772000
-    endYear = 2050
+    endYear = 2051
     currentYear = startYear
-    currentVolume = 24
+    currentVolume = 11
     lastVolume = currentVolume
     # prfloat(population(2000))
-    x, y, z, u = [], [], [], []
+    x, y, z, u, k = [], [], [], [], []
     for i in range(1980, 2022):
         z.append(i)
     u = [1201.572, 1200.94, 1199.958, 1210.452, 1207.838, 1207.21, 1203.94, 1208.608, 1209.64, 1198.822, 1188.626, 1178.496, 1178.814, 1190.058, 1187.23, 1180.646, 1193.016, 1198.242, 1213.408, 1210.822, 1211.166, 1193.33, 1171.276, 1150.758, 1136.776, 1142.93, 1137.444, 1124.254, 1113.34, 1105.758, 1099.238, 1095.512, 1127.992, 1116.864, 1100.082, 1083.592, 1079.646, 1086.114, 1085.662, 1087.878, 1095.45, 1082.08]
     while currentYear < endYear:
+        if currentYear % 1 == 0:
+            k.append(volume_to_elevation(currentVolume))
+            print(str(int(currentYear)) + " " + str(volume_to_elevation(currentVolume)) + " " + "")
         currentYear += step
         deltaYear = currentYear - startYear
         lastVolume = currentVolume
-        currentVolume += step * (inflow(currentYear) - outflow(currentYear) - evaporation(currentYear))
+        currentVolume += step * (inflow(currentYear) - outflow(currentYear) - 0.8 * evaporation(currentYear))
         x.append(currentYear)
         y.append(volume_to_elevation(currentVolume))
         if currentVolume == 0:
             break
     plt.plot(x, y)
     plt.plot(z, u)
+    plt.plot([1980], [1400])
+    plt.plot([1980], [900])
     plt.show()
